3cb3dd7070355ca77358c04bc5562ea494b18087
Ulrich Berthold <u@sansculotte.net>
2022-01-22 19:02:47 +0100

    display png single images in a loop

--- display.py
+++ display.py
@@ -4,30 +4,26 @@ import sys
 from glob import glob
 
 
-xs = glob('jesus/*.jpg')
+xs = glob('*.png')
 
 if not xs:
     print('no images found')
     sys.exit()
 
 i = 0
-images = [pygame.image.load(x) for x in xs]
+images = [pygame.image.load(x) for x in sorted(xs)]
+number_of_images = len(images)
 
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
+clock = pygame.time.Clock()
 
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                i = i - 1 if i > 0 else len(xs) - 1
-            if event.key == pygame.K_RIGHT:
-                i = i + 1 if i < len(xs) else 0
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-
     screen.fill((0, 0, 0))
     screen.blit(images[i], (0, 0))
     pygame.display.update()
+
+
+    dt = clock.tick(25)
+    if dt >= 1000 / 25:
+        i = (i + 1) % number_of_images
