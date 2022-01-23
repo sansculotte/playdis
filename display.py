5a50e094791e25c40ce2dbf9e450f9215a0c5a30
Ulrich Berthold <u@sansculotte.net>
2022-01-23 20:55:24 +0100

    proper code structure, commandline arguments and program exit

--- display.py
+++ display.py
@@ -1,29 +1,65 @@
+#!/usr/bin/env python
 from __future__ import print_function
+import argparse
 import pygame
 import sys
 from glob import glob
 
 
-xs = glob('*.png')
+def loop_image_sequence(screen, clock, images, number_of_images, fps):
+    i = 0
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-if not xs:
-    print('no images found')
-    sys.exit()
+        screen.fill((0, 0, 0))
+        screen.blit(images[i], (0, 0))
+        pygame.display.update()
 
-i = 0
-images = [pygame.image.load(x) for x in sorted(xs)]
-number_of_images = len(images)
-
-pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-clock = pygame.time.Clock()
-
-while 1:
-    screen.fill((0, 0, 0))
-    screen.blit(images[i], (0, 0))
-    pygame.display.update()
-
-
-    dt = clock.tick(25)
-    if dt >= 1000 / 25:
+        clock.tick(fps)
         i = (i + 1) % number_of_images
+
+
+def switch_image_sequence(screen, images, number_of_images):
+    pass
+
+
+def display_images(args, images, number_of_images):
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+    if args.play:
+        clock = pygame.time.Clock()
+        loop_image_sequence(screen, clock, images, number_of_images, args.fps)
+    else:
+        switch_image_sequence(screen, images, number_of_images)
+
+    pygame.quit()
+
+
+def load_images(xs):
+    images = [pygame.image.load(x) for x in sorted(xs)]
+    return images, len(images)
+
+
+def main(args):
+    xs = glob(args.images)
+    if not xs:
+        print('no images found')
+        sys.exit()
+
+    images, number_of_images = load_images(xs)
+    display_images(args, images, number_of_images)
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(
+        description='Display images on the framebuffer'
+    )
+    ap.add_argument('images',
+                    help='image files to display. may contain glob characters')
+    ap.add_argument('-f', '--fps', type=float, default=25)
+    ap.add_argument('-p', '--play', action='store_true', help='play image sequence')
+    args = ap.parse_args()
+    main(args)
